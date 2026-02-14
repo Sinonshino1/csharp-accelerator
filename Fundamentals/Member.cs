261ae9f8cc28af910e0ff253b308544b1a54aaf9
Ben Field <12pfieldbcoding@gmail.com>
2026-02-14 14:15:22 +0000

    Moved the Member class into its own dedicated Member.cs file for better structure

---
+++ Fundamentals/Member.cs
@@ -0,0 +1,16 @@
+class Member        // Creates the Member class, so we can have member objects to list in the other methods / store member objects
+{
+    public string Name { get; }     // Set a public scope, string property called Name. The { get } means that it is a read only variable, nad can't be adjusted outside of the constructor
+    public int Age { get; }     // Set a public scope, int property called Age. Takes the string value submitted in the console and converts it to an int value.
+
+    public Member(string name, int age)     // Set a public scope Object called Member with attributes name and int, with no methods
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public override string ToString()       // Replaces the default ToString function
+    {
+        return $"{Name}, Age {Age}";        // This is a return statement the string, with F strings being used to directly pull the objects attributes into the text
+    }
+}
